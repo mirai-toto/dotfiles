fdb218a10649617af71a431a89e9d127a505aab7
mirai-toto <miout-13@hotmail.fr>
2026-03-30 21:21:06 +0200

    feat: add gitlab.nvim and octo.nvim extras, update lazy-lock, optimize compinit

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,5 +1,24 @@
 return {
   {
+    "harrisoncramer/gitlab.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    build = function()
+      require("gitlab.server").build(true)
+    end,
+    opts = {},
+    keys = {
+      { "<leader>glr", "<cmd>lua require('gitlab').review()<cr>", desc = "GitLab Review" },
+      { "<leader>gls", "<cmd>lua require('gitlab').summary()<cr>", desc = "GitLab MR Summary" },
+      { "<leader>gla", "<cmd>lua require('gitlab').approve()<cr>", desc = "GitLab Approve" },
+      { "<leader>glp", "<cmd>lua require('gitlab').pipeline()<cr>", desc = "GitLab Pipeline" },
+    },
+  },
+  {
     "sindrets/diffview.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
@@ -10,3 +29,4 @@ return {
     },
   },
 }
+
