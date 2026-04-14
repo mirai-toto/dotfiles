b4cf554a57161eda0669efb4452ba1fc84454de1
mirai-toto <miout-13@hotmail.fr>
2026-04-14 15:01:41 +0200

    chore: unbind git log keybind

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -5,6 +5,7 @@ vim.keymap.set("n", "<leader>sp", function()
   require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } })
 end, { desc = "Search and Replace (current file)" })
 
+vim.keymap.del("n", "<leader>gl")
 local glab = require("glab")
 vim.keymap.set("n", "<leader>glp", glab.view_pipeline, { desc = "Pipeline CI (glab)" })
 vim.keymap.set("n", "<leader>glr", glab.run_pipeline, { desc = "Run pipeline (glab)" })
