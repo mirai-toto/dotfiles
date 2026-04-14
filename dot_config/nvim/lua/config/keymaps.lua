822588743c349474accfc58e85197b9b21babaac
mirai-toto <miout-13@hotmail.fr>
2026-04-14 14:05:18 +0200

    chore: search and replace current file keybind

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>sp", function()
+  require("grug-far").open({ prefills = { paths = vim.fn.expand("%") } })
+end, { desc = "Search and Replace (current file)" })
+
 local glab = require("glab")
-vim.keymap.set("n", "<leader>glP", glab.view_pipeline, { desc = "Pipeline CI (glab)" })
+vim.keymap.set("n", "<leader>glp", glab.view_pipeline, { desc = "Pipeline CI (glab)" })
 vim.keymap.set("n", "<leader>glR", glab.run_pipeline, { desc = "Run pipeline (glab)" })
